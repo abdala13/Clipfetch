e624dae56c4c46726c40f4f97f5abe3829c7b146
abdala13 <41635926+abdala13@users.noreply.github.com>
2026-05-09 02:32:25 +0300

    Delete src directory

--- src/db/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://nzmzifxdmdywtmyocqoz.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_udYWNb-qDgsRdaOkF8xsVA_0CiHtWza";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
